d5c1b278166cff5a9c370da35106d722ebe9386b
Igor Apolinário <igor_apolinario96@hotmail.com>
2020-02-24 13:50:32 -0300

    Adicionada estrutura para o projeto de portfolio

---
+++ src/data/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  defaultTitle: 'Igor Apolinário',
+  logo: 'https://igorapolinario.netlify.com/favicon/favicon-512.png',
+  author: 'Igor Apolinário',
+  url: 'https://igorapolinario.netlify.com/',
+  legalName: 'Igor Apolinário',
+  defaultDescription: 'I’m Igor Apolinário and I’m a Front-end Developer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/igoorapolinario/',
+    github: 'https://github.com/igor-apolinario/',
+    linkedin: 'https://www.linkedin.com/in/igor-apolinario/',
+    instagram: 'https://instagram.com/igor_apolinario',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'appId',
+    twitter: '@igoorapolinario',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2020',
+  recaptchaKey: '6Lcs6lQUAAAAAEwhNH2IsobIe2csdda4TU3efpMN',
+};
